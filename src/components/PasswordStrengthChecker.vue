80330743e15f3849274a07523389dcbf8602495e
Josh Hendrie <Josh.Hendrie101@gmail.com>
2024-05-23 18:49:30 +1200

    minor changes

--- src/components/PasswordStrengthChecker.vue
+++ src/components/PasswordStrengthChecker.vue
@@ -7,21 +7,26 @@
         <p>Email: {{ account.email }}</p>
         <p>
           Password: 
-          <span v-if="account.showPassword">{{ account.password }}</span>
-          <span v-else>••••••••</span>
-          <button @click="togglePasswordVisibility(account)">
+          <span v-if="!account.editing && account.showPassword">{{ account.password }}</span>
+          <span v-else-if="!account.editing">••••••••</span>
+          <span v-else>
+            <input v-model="account.updatedPassword" type="password">
+            <button @click="savePassword(account)">Save</button>
+          </span>
+          <button v-if="!account.editing" @click="togglePasswordVisibility(account)">
             <i :class="account.showPassword ? 'fa-solid fa-eye-slash' : 'fa-solid fa-eye'" :title="account.showPassword ? 'Hide' : 'Show'"></i>
           </button>
         </p>
-        <p>Password Strength: {{ getPasswordStrength(account.password) }}</p>
+        <p class="passStrField">Password Strength: {{ getPasswordStrength(account.password) }}</p>
         <div class="strength-bar" :class="getStrengthClass(account.password)"></div>
+        <button class="editBtn" v-if="!account.editing" @click="editPassword(account)">Edit</button>
       </div>
     </div>
   </template>
   
   <script setup>
   import { ref, onMounted, computed } from 'vue';
-  import { getDatabase, ref as dbRef, onValue } from 'firebase/database';
+  import { getDatabase, ref as dbRef, update, onValue } from 'firebase/database';
   import zxcvbn from 'zxcvbn'; // Import zxcvbn library
   
   const accounts = ref([]);
@@ -31,7 +36,7 @@
     const accountsRef = dbRef(database, 'accounts');
     onValue(accountsRef, (snapshot) => {
       const data = snapshot.val();
-      accounts.value = Object.keys(data).map(key => ({ id: key, ...data[key], showPassword: false }));
+      accounts.value = Object.keys(data).map(key => ({ id: key, ...data[key], showPassword: false, editing: false, updatedPassword: '' }));
     });
   });
   
@@ -59,6 +64,21 @@
     account.showPassword = !account.showPassword;
   };
   
+  const editPassword = (account) => {
+    account.editing = true;
+    account.updatedPassword = account.password;
+  };
+  
+  const savePassword = (account) => {
+    account.password = account.updatedPassword;
+    account.editing = false;
+  
+    // Update the password in the database
+    update(dbRef(database, `accounts/${account.id}`), {
+      password: account.password
+    });
+  };
+  
   // Computed property to sort accounts by password strength
   const sortedAccounts = computed(() => {
     return [...accounts.value].sort((a, b) => {
@@ -80,15 +100,27 @@
   };
   </script>
   
+
+
   <style scoped>
   .password-strength-checker {
-    margin-top:200px;
-    margin-left:400px;
+    margin-top: 200px;
+    margin-left:600px;
   }
   
+.passStrField {
+    margin-bottom: 0px;
+}
+
+.editBtn {
+    margin-top: 10px;
+}
+
   .strength-bar {
     height: 10px;
-    max-width: 200px;
+    
+    max-width: 300px;
+
   }
   
   .strength-very-weak {
